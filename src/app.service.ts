12c6d111246b8bbd2215e046c34c0fe83600411f
Luis Furtado <luisgustavolf@gmail.com>
2025-09-28 18:02:46 -0300

    fix: update deployment workflow and configuration files for NestJS app

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import * as packageJson from 'package.json';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Nest!';
+    return `Hello Nest v${packageJson.version}`;
   }
 }
